06fbdab5ef86143599cecd11cf73967ca6046449
rolah <nick1@freemail.hu>
2015-02-13 16:34:13 +0100

    Project euler problems 1 to 5

---
+++ problem_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            int NumbersSquare_Sum = 0;
+            int SumNumbers_Square = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                NumbersSquare_Sum += i * i;
+                SumNumbers_Square += i;
+            }
+
+            SumNumbers_Square = SumNumbers_Square * SumNumbers_Square;
+
+            Console.WriteLine(SumNumbers_Square - NumbersSquare_Sum);
+            Console.ReadLine();
+        }
+    }
+}
